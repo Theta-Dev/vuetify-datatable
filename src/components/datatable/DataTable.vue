bbe8a078f72de9b83f8c16b86d506ff8a5d89463
Theta-Dev <t.testboy@gmail.com>
2021-03-28 20:07:12 +0200

    Added search bar and filter buttons

--- src/components/datatable/DataTable.vue
+++ src/components/datatable/DataTable.vue
@@ -1,40 +1,67 @@
 <template>
-  <v-simple-table>
-    <template v-slot:default>
-      <thead>
-      <tr>
-        <th
-          v-for="name in Object.keys(fields)"
-          :key="name"
-        >{{ name }}
-        </th>
-      </tr>
-      </thead>
-      <tbody>
-      <template v-for="(item, index) in data">
-        <tr
-          v-for="(n, i) in numItemRows[index]"
-          :key="index + '.' + i"
-          v-show="visibleItems[index]"
-        >
-          <template
-            v-for="(field, fname, fi) in fields"
+  <v-card>
+    <v-card-title>
+      DataTable
+    </v-card-title>
+    <v-card-text>
+      <v-text-field
+        v-model="search"
+        label="Search..."
+        prepend-icon="mdi-magnify"
+        clearable
+        hide-details
+        single-line
+      />
+    </v-card-text>
+    <v-simple-table>
+      <template v-slot:default>
+        <thead>
+        <tr>
+          <th
+            v-for="(name, i) in Object.keys(fields)"
+            :key="name"
           >
-            <component
-              v-if="(numItemRows[index] - item[fi].length) === 0 || i===0"
-              :key="fname"
-              :is="field"
-              :rspan="(numItemRows[index] - item[fi].length) === 0 ? 1 : numItemRows[index]"
-              :val="item[fi][i]"
-              :filter="filter"
-              :search="search"
-            />
-          </template>
+            <v-hover v-slot="{ hover }">
+              <v-row no-gutters align="center">
+                <v-col>
+                  {{ name }}
+                </v-col>
+                <v-col class="text-right">
+                  <v-btn icon :color="getFilterButtonColor(i, hover)">
+                    <v-icon>mdi-filter</v-icon>
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </v-hover>
+          </th>
         </tr>
+        </thead>
+        <tbody>
+        <template v-for="(item, index) in data">
+          <tr
+            v-for="(n, i) in numItemRows[index]"
+            :key="index + '.' + i"
+            v-show="visibleItems[index]"
+          >
+            <template
+              v-for="(field, fname, fi) in fields"
+            >
+              <component
+                v-if="(numItemRows[index] - item[fi].length) === 0 || i===0"
+                :key="fname"
+                :is="field"
+                :rspan="(numItemRows[index] - item[fi].length) === 0 ? 1 : numItemRows[index]"
+                :val="item[fi][i]"
+                :filter="filter"
+                :search="search"
+              />
+            </template>
+          </tr>
+        </template>
+        </tbody>
       </template>
-      </tbody>
-    </template>
-  </v-simple-table>
+    </v-simple-table>
+  </v-card>
 </template>
 
 <script>
@@ -57,7 +84,7 @@ export default {
         ['Fix: API error'],
       ],
     ],
-    filter: [[], []],
+    filter: [],
     search: '',
   }),
 
@@ -107,12 +134,13 @@ export default {
       return this.data.map((it, i) => {
         // Does search match
         if(this.search) {
+          const search = this.search.toLowerCase();
           // Go through all fields of item
           // At least one searchable has to match
           const searchMatched = this.searchables[i].some(
             (fl) => {
               if(!Array.isArray(fl) || fl.length === 0) return true;
-              return fl.some((fli) => fli.includes(this.search));
+              return fl.some((fli) => fli.includes(search));
             },
           );
           if(!searchMatched) return false;
@@ -132,11 +160,16 @@ export default {
     },
   },
 
-  methods: {},
+  methods: {
+    getFilterButtonColor(i, hover) {
+      if(Array.isArray(this.filter[i]) && this.filter[i].length > 0) return 'primary';
+      return hover ? '' : 'transparent';
+    },
+  },
 
   mounted() {
     // console.log(this.fields);
-    console.log(this.filterLists);
+    // console.log(this.filterLists);
   },
 };
 </script>
@@ -151,7 +184,7 @@ tr:not(:last-child) > th:not(.v-data-table__mobile-row) {
 
 .theme--dark.v-data-table > .v-data-table__wrapper > table > tbody >
 tr > td:not(.v-data-table__mobile-row),
-.theme--dark.v-data-table >  .v-data-table__wrapper > table > tbody >
+.theme--dark.v-data-table > .v-data-table__wrapper > table > tbody >
 tr > th:not(.v-data-table__mobile-row) {
   border-bottom: thin solid rgba(255, 255, 255, 0.12);
 }
