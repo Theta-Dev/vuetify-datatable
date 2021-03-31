3aca86f05d53875631a251430ae39dda6ade9c5e
Theta-Dev <t.testboy@gmail.com>
2021-03-31 09:41:16 +0200

    Disabled sort for dummy

--- src/components/datatable/TableFieldDummy.js
+++ src/components/datatable/TableFieldDummy.js
@@ -1,6 +1,11 @@
 import TableField from './TableField';
 
 export default class TableFieldDummy extends TableField {
+  constructor(name) {
+    super(name);
+    this.sortEnabled = false;
+  }
+
   getSearchable() {
     return null;
   }
